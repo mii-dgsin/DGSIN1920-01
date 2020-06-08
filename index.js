78f10bbea485c6aaf2a1be0783be5b78518b4e4c
Jose Antonio Gomez Romero <jagr511@gmail.com>
2020-06-08 19:45:25 +0200

    First version static server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var express = require("express");
+var path = require('path');
 var app = express();
 app.get("/hello", (req, res) => {
     res.send("hello world");
 });
-app.listen(process.env.PORT || 8080);
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT || 8080).on("error",(e)=>{
+    console.error("Server NOT ready!");
+});
+
+console.log("Server ready");
