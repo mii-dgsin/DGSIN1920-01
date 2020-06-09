3ed9db07433cc97d775c43b38894295ddb9dbb3b
Jose Antonio Gomez Romero <jagr511@gmail.com>
2020-06-09 20:05:27 +0200

    2 Version of Backend

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var app = express();
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bp.json());
 
-var initialContacts = [
+var initialCollection = [
     {
         "name": "John",
         "email": "john.doe@example.com",
@@ -46,23 +46,23 @@ MongoClient.connect(mdbURL, (err, client) => {
         console.error("DB connection error: " + err);
         process.exit(1);
     } else {
-        db = client.db("dgsin-xx-db").collection("contacts");
-        db.find({}).toArray((err, contacts) => {
+        db = client.db("dgsin-xx-db").collection("collection");
+        db.find({}).toArray((err, collection) => {
             if (err) {
                 console.error("Error getting data from DB: " + err);
-            } else if (contacts.length == 0) {
-                console.info("Adding initial contacts to empty DB");
-                db.insert(initialContacts);
+            } else if (collection.length == 0) {
+                console.info("Adding initial collection to empty DB");
+                db.insert(initialCollection);
             } else {
-                console.info("Connected to the DB with " + contacts.length + " contacts");
+                console.info("Connected to the DB with " + collection.length + " collection");
             }
         });
     }
 });
 
-// Get rid of _id when returning contacts
-function formatContacts (contacts) {
-    return contacts.map((contact) => {
+// Get rid of _id when returning collection
+function formatCollection (collection) {
+    return collection.map((contact) => {
         delete contact._id // removes the property
         return contact;
     });
@@ -70,38 +70,38 @@ function formatContacts (contacts) {
 
 
 // GET a collection
-app.get(BASE_API + "/contacts", (req, res) => {
-    console.info("New GET request to /contacts");
-    db.find({}).toArray((err, contacts) => {
+app.get(BASE_API + "/collection", (req, res) => {
+    console.info("New GET request to /collection");
+    db.find({}).toArray((err, collection) => {
         if (err) {
             console.error("Error getting data from DB: " + err);
             res.sendStatus(500);
         } else {
-            var formattedContacts = formatContacts(contacts);
-            console.debug("Sending contacts: " + JSON.stringify(formattedContacts, null, 2));
-            res.send(formattedContacts);
+            var formattedCollection = formatCollection(collection);
+            console.debug("Sending collection: " + JSON.stringify(formattedCollection, null, 2));
+            res.send(formattedCollection);
         }
     });
 });
 
 // POST over a collection
-app.post(BASE_API + "/contacts", (req, res) => {
+app.post(BASE_API + "/collection", (req, res) => {
     var newContact = req.body;
     if (!newContact) {
-        console.warn("New POST request to /contacts/ without contact, sending 400...");
+        console.warn("New POST request to /collection/ without contact, sending 400...");
         res.sendStatus(400); //bad request
     } else {
-        console.info("New POST request to /contacts with body: " + JSON.stringify(newContact, null, 2));
+        console.info("New POST request to /collection with body: " + JSON.stringify(newContact, null, 2));
         if (!newContact.name || !newContact.email || !newContact.phone) {
             console.warn("The contact " + JSON.stringify(newContact, null, 2) + " is not well-formed, sending 422...");
             res.sendStatus(422); // unprocessable entity
         } else {
-            db.find({ "name": newContact.name }).toArray((err, contacts) => {
+            db.find({ "name": newContact.name }).toArray((err, collection) => {
                 if (err) {
                     console.error("Error getting data from DB: " + err);
                     res.sendStatus(500);
                 } else {
-                    if (contacts.length > 0) {
+                    if (collection.length > 0) {
                         console.warn("The contact " + JSON.stringify(newContact, null, 2) + " already exists, sending 409...");
                         res.sendStatus(409); // conflict
                     } else {
@@ -116,8 +116,8 @@ app.post(BASE_API + "/contacts", (req, res) => {
 });
 
 // DELETE over a collection
-app.delete(BASE_API + "/contacts", (req, res) => {
-    console.info("New DELETE request to /contacts");
+app.delete(BASE_API + "/collection", (req, res) => {
+    console.info("New DELETE request to /collection");
     db.remove({}, { multi: true }, (err, result) => {
         if (err) {
             console.error('Error removing data from DB');
@@ -125,10 +125,10 @@ app.delete(BASE_API + "/contacts", (req, res) => {
         } else {
             var numRemoved = result.result.n;
             if (numRemoved === 0) {
-                console.warn("There are no contacts to delete");
+                console.warn("There are no collection to delete");
                 res.sendStatus(404); // not found
             } else {
-                console.debug("All the contacts (" + numRemoved + ") have been succesfully deleted, sending 204...");
+                console.debug("All the collection (" + numRemoved + ") have been succesfully deleted, sending 204...");
                 res.sendStatus(204); // no content
             }
         }
@@ -136,26 +136,26 @@ app.delete(BASE_API + "/contacts", (req, res) => {
 });
 
 // PUT over a collection
-app.put(BASE_API + "/contacts", (req, res) => {
-    console.warn("New PUT request to /contacts, sending 405...");
+app.put(BASE_API + "/collection", (req, res) => {
+    console.warn("New PUT request to /collection, sending 405...");
     res.sendStatus(405); // method not allowed
 });
 
 // GET a specific resource
-app.get(BASE_API + "/contacts/:name", (req, res) => {
+app.get(BASE_API + "/collection/:name", (req, res) => {
     var name = req.params.name;
     if (!name) {
-        console.warn("New GET request to /contacts/:name without name, sending 400...");
+        console.warn("New GET request to /collection/:name without name, sending 400...");
         res.sendStatus(400); // bad request
     } else {
-        console.info("New GET request to /contacts/" + name);
-        db.find({ "name": name }).toArray((err, filteredContacts) => {
+        console.info("New GET request to /collection/" + name);
+        db.find({ "name": name }).toArray((err, filteredCollection) => {
             if (err) {
                 console.error('Error getting data from DB');
                 res.sendStatus(500); // internal server error
             } else {
-                if (filteredContacts.length > 0) {
-                    var contact = formatContacts(filteredContacts)[0]; //since we expect to have exactly ONE contact with this name
+                if (filteredCollection.length > 0) {
+                    var contact = formatCollection(filteredCollection)[0]; //since we expect to have exactly ONE contact with this name
                     console.debug("Sending contact: " + JSON.stringify(contact, null, 2));
                     res.send(contact);
                 } else {
@@ -168,32 +168,32 @@ app.get(BASE_API + "/contacts/:name", (req, res) => {
 });
 
 // POST a specific resource
-app.post(BASE_API + "/contacts/:name", (req, res) => {
+app.post(BASE_API + "/collection/:name", (req, res) => {
     var name = req.params.name;
-    console.warn("New POST request to /contacts/" + name + ", sending 405...");
+    console.warn("New POST request to /collection/" + name + ", sending 405...");
     res.sendStatus(405); // method not allowed
 });
 
 // DELETE a specific resource
-app.delete(BASE_API + "/contacts/:name", (req, res) => {
+app.delete(BASE_API + "/collection/:name", (req, res) => {
     var name = req.params.name;
     if (!name) {
-        console.warn("New DELETE request to /contacts/:name without name, sending 400...");
+        console.warn("New DELETE request to /collection/:name without name, sending 400...");
         res.sendStatus(400); // bad request
     } else {
-        console.info("New DELETE request to /contacts/" + name);
+        console.info("New DELETE request to /collection/" + name);
         db.remove({ name: name }, {}, function (err, result) {
             if (err) {
                 console.error('Error removing data from DB');
                 res.sendStatus(500); // internal server error
             } else {
                 var numRemoved = result.result.n;
-                console.debug("Contacts removed: " + numRemoved);
+                console.debug("Collection removed: " + numRemoved);
                 if (numRemoved === 1) {
                     console.debug("The contact with name " + name + " has been succesfully deleted, sending 204...");
                     res.sendStatus(204); // no content
                 } else {
-                    console.warn("There are no contacts to delete");
+                    console.warn("There are no collection to delete");
                     res.sendStatus(404); // not found
                 }
             }
@@ -202,27 +202,27 @@ app.delete(BASE_API + "/contacts/:name", (req, res) => {
 });
 
 // PUT over a specific resource
-app.put(BASE_API + "/contacts/:name", (req, res) => {
+app.put(BASE_API + "/collection/:name", (req, res) => {
     var name = req.params.name;
     var updatedContact = req.body;
     if (!name) {
-        console.warn("New PUT request to /contacts/:name without name, sending 400...");
+        console.warn("New PUT request to /collection/:name without name, sending 400...");
         res.sendStatus(400); // bad request
     } else if (!updatedContact) {
-        console.warn("New PUT request to /contacts/ without contact, sending 400...");
+        console.warn("New PUT request to /collection/ without contact, sending 400...");
         res.sendStatus(400); // bad request
     } else {
-        console.info("New PUT request to /contacts/" + name + " with data " + JSON.stringify(updatedContact, null, 2));
+        console.info("New PUT request to /collection/" + name + " with data " + JSON.stringify(updatedContact, null, 2));
         if (!updatedContact.name || !updatedContact.phone || !updatedContact.email) {
             console.warn("The contact " + JSON.stringify(updatedContact, null, 2) + " is not well-formed, sending 422...");
             res.sendStatus(422); // unprocessable entity
         } else {
-            db.find({ "name": name }).toArray((err, contacts) => {
+            db.find({ "name": name }).toArray((err, collection) => {
                 if (err) {
                     console.error('Error getting data from DB');
                     res.sendStatus(500); // internal server error
                 } else {
-                    if (contacts.length > 0) {
+                    if (collection.length > 0) {
                         db.update({ name: name }, updatedContact);
                         console.debug("Modifying contact with name " + name + " with data " + JSON.stringify(updatedContact, null, 2));
                         res.send(updatedContact); // return the updated contact
