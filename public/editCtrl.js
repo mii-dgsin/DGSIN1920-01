4df2fb124eef07d2d714f600e17dc1c5db2fc185
Jose Antonio Gomez Romero <jagr511@gmail.com>
2020-06-11 08:58:54 +0200

    Last version Frontend

--- public/editCtrl.js
+++ public/editCtrl.js
@@ -2,16 +2,16 @@ angular.module("CollectionManagerApp")
     .controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function ($scope, $http, $routeParams, $location) {
         console.log("EditCtrl initialized.");
         var collectionName = $routeParams.name;
-        var APIurl = "/api/v1/collections/" + collectionName;
+        var APIurl = "/api/v1/collection/" + collectionName;
 
-        function getCollections() {
+        function getCollection() {
             $http.get(APIurl).then(function (response) {
                 console.log("Collection to be updated: " + JSON.stringify(response.data, null, 2));
                 $scope.updatedCollection = response.data;
             });
         }
 
-        getCollections();
+        getCollection();
 
         $scope.updateCollection = function updateCollection() {
             $http.put(APIurl, $scope.updatedCollection).then(function (response) {
--- public/listCtrl.js
+++ public/listCtrl.js
@@ -3,33 +3,33 @@ angular.module("CollectionManagerApp")
         console.log("ListCtrl initialized.");
         var APIurl = "/api/v1/collection";
         
-        function getCollection () {
+        function getCollections () {
             $http.get(APIurl).then(function (response) {
-                console.log("Collection received: " + JSON.stringify(response.data, null, 2));
-                $scope.collection = response.data;
+                console.log("Collections received: " + JSON.stringify(response.data, null, 2));
+                $scope.collections = response.data;
             });
         }
 
         $scope.addCollection = function addCollection() {
             $http.post(APIurl, $scope.newCollection).then(function (response) {
                 console.log("POST finished");
-                getCollection();
+                getCollections();
             }); 
         }
 
         $scope.deleteCollection = function deleteCollection(CollectionName) {
             $http.delete(APIurl+"/"+CollectionName).then(function (response) {
                 console.log("DELETE finished");
-                getCollection();
+                getCollections();
             }); 
         }
 
         $scope.deleteCollections = function deleteCollections() {
             $http.delete(APIurl).then(function (response) {
-                console.log("DELETE all collection finished");
-                getCollection();
+                console.log("DELETE all collections finished");
+                getCollections();
             }); 
         }
 
-        getCollection();
+        getCollections();
     }]);
