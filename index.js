9ea49535ffa5490138e268ea54c81b01f2d27458
Jose Antonio Gomez Romero <jagr511@gmail.com>
2020-06-14 10:49:18 +0200

    Integraciones

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var path = require('path');
 var bp = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-const mdbURL = "mongodb+srv://admin:Jose1973@dgsin1920-01-knvtu.mongodb.net/dgsin-01-db?retryWrites=true&w=majority";
+const mdbURL = "mongodb+srv://admin:Jose1973@dgsin1920-01-knvtu.mongodb.net/dgsin-01-db2?retryWrites=true&w=majority"; 
+             
 
 var app = express();
 
@@ -44,10 +45,10 @@ MongoClient.connect(mdbURL, (err, client) => {
     }
 });
 
-const BASE_API_PATH = "/api/v1";
+/*const BASE_API_PATH = "/api/v1";
 // GET a collection for widget visualization 
 app.get(BASE_API_PATH + "/data", function (req, res){
 	console.log("INFO: New GET request to /data");
  
     res.send([2, 3, 4, 1, 2, 7, 13, 8, 4, 29, 1]);
-});
+});*/
--- public/analyticsCtrl.js
+++ public/analyticsCtrl.js
@@ -5,83 +5,82 @@ angular
 	
 		$scope.update = function (){
 			$http 
-				.get("/api/v1/data")
+				.get("/api/v1/collection")
 				.then(function (response) {
-					$scope.data = response.data;
-					console.log("Data received:" + $scope.data); 
-				}); 
-		}
-		$scope.update(); 
+					//$scope.data = response.data;
+					console.log("Data received:" + $scope.data);
+					Highcharts.chart('container', {
 
-		Highcharts.chart('container', {
-
-			title: {
-				text: 'Solar Employment Growth by Sector, 2010-2016'
-			},
-		
-			subtitle: {
-				text: 'Source: thesolarfoundation.com'
-			},
-		
-			yAxis: {
-				title: {
-					text: 'Number of Employees'
-				}
-			},
-		
-			xAxis: {
-				accessibility: {
-					rangeDescription: 'Range: 2010 to 2017'
-				}
-			},
-		
-			legend: {
-				layout: 'vertical',
-				align: 'right',
-				verticalAlign: 'middle'
-			},
-		
-			plotOptions: {
-				series: {
-					label: {
-						connectorAllowed: false
-					},
-					pointStart: 2010
-				}
-			},
-		
-			series: [{
-				name: 'Installation',
-				data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-			}, {
-				name: 'Manufacturing',
-				data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-			}, {
-				name: 'Sales & Distribution',
-				data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-			}, {
-				name: 'Project Development',
-				data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-			}, {
-				name: 'Other',
-				data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-			}],
-		
-			responsive: {
-				rules: [{
-					condition: {
-						maxWidth: 500
-					},
-					chartOptions: {
+						title: {
+							text: 'World University Ranking'
+						},
+					
+						subtitle: {
+							text: 'DATASET BY HOZEFA HAVELIWALA'
+						},
+					
+						yAxis: {
+							title: {
+								text: 'Rating (0-100)'
+							},
+							categories: response.data.map(function (d){
+								return d.total_score;
+							})
+						},
+					
+						xAxis: {
+							title: {
+								text: 'University',
+							},
+							categories: response.data.map(function (d){
+								return d.name;
+							})
+							
+						},
+						
+						
+					
 						legend: {
-							layout: 'horizontal',
-							align: 'center',
-							verticalAlign: 'bottom'
+							layout: 'vertical',
+							align: 'right',
+							verticalAlign: 'middle'
+						},
+					
+						
+						series: [{
+							name: 'Teaching Rating',
+							data: response.data.map(function (d){
+								return d.teaching_rating;
+							})},{
+							
+								name: 'Industry Income Rating',
+							data: response.data.map(function (d){
+								return d.industry_income_rating;
+							
+							})},{
+							
+								name: 'total_score',
+							data: response.data.map(function (d){
+								return d.total_score;
+							})}
+						],
+					
+						responsive: {
+							rules: [{
+								
+								chartOptions: {
+									legend: {
+										layout: 'horizontal',
+										align: 'center',
+										verticalAlign: 'bottom'
+									}
+								}
+							}]
 						}
-					}
-				}]
-			}
-		
-		});
+					
+					});
 
+					});
+				}		
+		$scope.update(); 
     }]);
